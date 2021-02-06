11f12ace175d1b3c8605b5c15e7ae9c07b06c6c1
Nidol <l>
2021-02-07 03:39:58 +0800

    完成较为完善的任务动画

--- Assets/PolygonTown/Scripts/PlayerController.cs
+++ Assets/PolygonTown/Scripts/PlayerController.cs
@@ -31,6 +31,14 @@ public class PlayerController : MonoBehaviour {
 
 	public bool isGround;
 
+	private Animator Anim;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		Anim = GetComponent<Animator>();
+	}
+
 	// Use this for initialization
 	private void Start () {
 		cc = GetComponent<CharacterController> ();
@@ -38,6 +46,7 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void Update () {
+		
 		
 		isGround = Physics.CheckSphere (groundCheck.position, checkRadius, groundLayer);
 		if (isGround && velocity.y < 0) {
@@ -50,11 +59,32 @@ public class PlayerController : MonoBehaviour {
 		dir = transform.forward * verticalMove + transform.right * horizontalMove;
 		// 移动player
 		cc.Move(dir * Time.deltaTime*3/2);
+	
 
-		if (Input.GetButton ("Jump") && isGround) {
+		if (Input.GetButton ("Jump")) {
 			velocity.y = jumpSpeed;
 		}
+		if (Input.GetKeyDown (KeyCode.W)) {
+			Anim.SetBool ("isWalk", true);
+		} else if(Input.GetKeyUp (KeyCode.W)){
+			Anim.SetBool ("isWalk", false);
+		}
 
+		if(Input.GetKeyDown (KeyCode.Space)) {
+			Anim.SetBool ("isJump", true);
+		}else if(Input.GetKeyUp (KeyCode.Space)){
+			Anim.SetBool ("isJump", false);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			Anim.SetBool ("isRun", true);
+		} else if(Input.GetKeyUp (KeyCode.LeftShift)){
+			Anim.SetBool ("isRun", false);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			Anim.SetBool ("isSetDown", true);
+		} else if(Input.GetKeyUp (KeyCode.LeftControl)){
+			Anim.SetBool ("isSetDown", false);
+		}
 		velocity.y -= gravity * Time.deltaTime;
 		cc.Move (velocity * Time.deltaTime);
 	}
